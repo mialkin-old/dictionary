f9da34cd773b3fbe091c66587b965a19ad8eaa19
Aleksei Mialkin <anmyalkin@gmail.com>
2019-11-30 12:12:09 +0300

    DictionaryDb

---
+++ src/Dictionary.Database/DictionaryDb.cs
@@ -0,0 +1,15 @@
+﻿using Dictionary.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dictionary.Database
+{
+    internal class DictionaryDb : DbContext
+    {
+        public DbSet<Word> Words { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=dictionary.db");
+        }
+    }
+}
