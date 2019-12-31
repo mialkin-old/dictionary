b7347adee1bd9a3254d1ccaf5b62dc4cb768b015
Aleksei Mialkin <anmyalkin@gmail.com>
2019-12-31 06:46:36 +0300

    FiltersExtensionMethods

--- src/Dictionary.Database/Repositories/Word/WordRepository.cs
+++ src/Dictionary.Database/Repositories/Word/WordRepository.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Dictionary.Database.ExtensionMethods;
 using Dictionary.Database.Models;
-using Dictionary.Shared.ExtensionMethods;
 using Dictionary.Shared.Filters.Word;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +15,7 @@ namespace Dictionary.Database.Repositories.Word
         }
         public DictionaryDb Db { get; }
 
+        // Подумать над вынесением в RepositoryBase.
         public async Task CreateAsync(WordDto word)
         {
             Db.Add(word);
@@ -24,19 +24,8 @@ namespace Dictionary.Database.Repositories.Word
 
         public async Task<IList<WordDto>> ListAsync(WordListFilter filter)
         {
-            IQueryable<WordDto> query = Db.Words
-                .Where(x => x.LanguageId == filter.LanguageId)
-                .AsQueryable();
-
-            if (!string.IsNullOrWhiteSpace(filter.OrderByPropertyName))
-            {
-                query = filter.OrderByDescending ?
-                    query.OrderByPropertyNameDescending(filter.OrderByPropertyName) :
-                    query.OrderByPropertyName(filter.OrderByPropertyName);
-            }
-
-            var result = await query
-                .Take(filter.Take)
+            var result = await Db.Words
+                .ApplyWordListFilter(filter)
                 .ToListAsync();
 
             return result;
