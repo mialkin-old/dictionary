fa61d51c74f5aef06012a6d6dd0db4701172138c
Aleksei Mialkin <anmyalkin@gmail.com>
2020-01-03 23:22:08 +0300

    Removes sematic

--- src/Dictionary.WebUi/Controllers/HomeController.cs
+++ src/Dictionary.WebUi/Controllers/HomeController.cs
@@ -1,39 +1,12 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using AutoMapper;
-using Dictionary.Services.Models.Word;
-using Dictionary.Services.Services.Word;
-using Dictionary.Shared.Filters.Word;
-using Dictionary.WebUi.ViewModels.Home;
-using Dictionary.WebUi.ViewModels.Word;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Dictionary.WebUi.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IWordService _wordService;
-        private readonly IMapper _mapper;
-
-        public HomeController(IWordService wordService, IMapper mapper)
+        public IActionResult Index()
         {
-            _wordService = wordService;
-            _mapper = mapper;
-        }
-        public async Task<IActionResult> Index(WordListFilter filter)
-        {
-            filter.LanguageId = 2;
-            filter.OrderByDescending = true;
-            filter.OrderByPropertyName = "Created";
-
-            IList<WordListServiceModel> words = await _wordService.ListAsync(filter);
-
-            var model = new HomeIndexViewModel
-            {
-                Words = _mapper.Map<List<WordListViewModel>>(words)
-            };
-
-            return View(model);
+            return View();
         }
     }
 }
