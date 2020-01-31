f5a61db257e34adbbdb8b04884acf6243a141e3c
Aleksei Mialkin <anmyalkin@gmail.com>
2020-01-31 23:02:10 +0300

    Saving words

--- src/Dictionary.WebUi/ClientApp/src/components/main/Main.js
+++ src/Dictionary.WebUi/ClientApp/src/components/main/Main.js
@@ -5,19 +5,27 @@ export class Main extends Component {
 
     state = {
         languageId: 2,
-        word: '',
-        saveButtonDisabled: true
+        name: '',
+        transcription: '',
+        translation: ''
     }
 
     constructor(props) {
         super(props);
 
         this.handleLanguageChange = this.handleLanguageChange.bind(this);
-        this.handleSearchbarChange = this.handleSearchbarChange.bind(this);
-        this.handleSearchbarKeyDown = this.handleSearchbarKeyDown.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleNameInputKeyDown = this.handleNameInputKeyDown.bind(this);
+        this.handleTranscriptionChange = this.handleTranscriptionChange.bind(this);
+        this.handleTranslationChange = this.handleTranslationChange.bind(this);
+
+        this.handleSave = this.handleSave.bind(this);
     }
 
     render() {
+        let saveDisabled =
+            !(this.state.name.length > 0 && this.state.translation.length > 0);
+
         return (
             <div>
                 <select id="language-select"
@@ -26,21 +34,23 @@ export class Main extends Component {
                     <option value={1}>English</option>
                     <option value={4}>Русский</option>
                 </select>
-                <input id="word"
-                    value={this.state.word}
-                    onChange={this.handleSearchbarChange}
-                    onKeyDown={this.handleSearchbarKeyDown}
+                <input id="name"
+                    onChange={(this.handleNameChange)}
+                    onKeyDown={this.handleNameInputKeyDown}
                     placeholder="Слово"></input>
                 <input id="transcription"
+                    onChange={(this.handleTranscriptionChange)}
                     placeholder="Транскрипция"></input>
                 <textarea id="translation"
                     placeholder="Перевод"
+                    onChange={(this.handleTranslationChange)}
                     rows="5"></textarea>
                 <button id="save-btn"
-                    disabled={this.state.saveButtonDisabled}>Сохранить</button>
+                    disabled={saveDisabled}
+                    onClick={this.handleSave}>Сохранить</button>
                 <Words
                     languageId={this.state.languageId}
-                    searchTerm={this.state.word} />
+                    searchTerm={this.state.name} />
             </div>);
     }
 
@@ -50,17 +60,45 @@ export class Main extends Component {
         });
     }
 
-    handleSearchbarChange(e) {
+    handleNameChange(e) {
         this.setState({
-            word: e.target.value
+            name: e.target.value
         });
     }
 
-    handleSearchbarKeyDown(e) {
+    handleNameInputKeyDown(e) {
         if (e.keyCode === 27) {
             this.setState({
-                word: ''
+                name: ''
             });
         }
+    }
+
+    handleTranscriptionChange(e) {
+        this.setState({
+            transcription: e.target.value
+        });
+    }
+
+    handleTranslationChange(e) {
+        this.setState({
+            translation: e.target.value
+        });
+    }
+
+    handleSave() {
+        fetch('word/create', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                LanguageId: this.state.languageId,
+                Name: this.state.name,
+                Transcription: this.state.transcription,
+                Translation: this.state.translation
+            })
+        })
     }
 }
