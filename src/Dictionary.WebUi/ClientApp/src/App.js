ff6a8bbe05cefc7a69dd883b080571445ded56b1
Aleksei Mialkin <anmyalkin@gmail.com>
2020-06-14 00:31:38 +0300

    JavaScript code cleanup

--- src/Dictionary.WebUi/ClientApp/src/App.js
+++ src/Dictionary.WebUi/ClientApp/src/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import React, {Component} from 'react';
+import {Route} from 'react-router';
+import {Layout} from './components/Layout';
+import 'antd/dist/antd.css';
 import './common.css'
-import { Main } from './components/main/Main';
-
+import {Main} from './components/Main';
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/main' component={Main} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Route path='/' component={Main}/>
+            </Layout>
+        );
+    }
 }
