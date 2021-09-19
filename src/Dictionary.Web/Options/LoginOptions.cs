ad2bd58960448ec798455fa83db7f91e6361a452
Aleksei Mialkin <anmyalkin@gmail.com>
2021-09-19 21:31:24 +0300

    Return annotations back

--- src/Dictionary.Web/Options/LoginOptions.cs
+++ src/Dictionary.Web/Options/LoginOptions.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Dictionary.Web.Options
 {
     public class LoginOptions
     {
         public const string Login = "Login";
+
+        [Required(AllowEmptyStrings = false)]
         public string? Username { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
         public string? Password { get; set; }
     }
 }
