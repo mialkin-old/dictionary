4ca123e2017b2c6c8da50c8544ae150d30bb653f
Aleksei Mialkin <anmyalkin@gmail.com>
2021-09-19 21:18:33 +0300

    Remove [Required] from Options

--- src/Dictionary.Web/Options/LoginOptions.cs
+++ src/Dictionary.Web/Options/LoginOptions.cs
@@ -1,15 +1,10 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Dictionary.Web.Options
 {
     public class LoginOptions
     {
         public const string Login = "Login";
-
-        [Required(AllowEmptyStrings = false)]
         public string? Username { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
         public string? Password { get; set; }
     }
 }
